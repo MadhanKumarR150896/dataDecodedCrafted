a6132f54900ecac874eceef9b21d696d90c3347b
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-12-16 14:02:22 +0530

    Header with responsive menu and Footer

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,1 +1,31 @@
-console.log('HI');
+
+const headerRight = document.querySelector('.header-right');
+const menuButton = document.querySelector('.menu-button');
+
+const openMenu = () => {
+  headerRight.classList.add('visible');
+  headerRight.setAttribute('aria-hidden', 'false');
+  menuButton.setAttribute('aria-expanded', 'true');
+}
+
+const closeMenu = () => {
+  headerRight.classList.remove('visible');
+  headerRight.setAttribute('aria-hidden', 'true');
+  menuButton.setAttribute('aria-expanded', 'false');
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.closest('.menu-button') && !headerRight.classList.contains('visible')) {
+    openMenu();
+    return;
+  }
+  if ((event.target.closest('.close-button') || event.target.closest('.main')) && headerRight.classList.contains('visible')) {
+    closeMenu();
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && headerRight.classList.contains('visible')) {
+    closeMenu();
+  }
+});
