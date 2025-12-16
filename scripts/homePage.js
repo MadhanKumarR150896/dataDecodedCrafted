b49fedd75bf9898871a167484085aa7821e8a8a0
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-12-16 14:58:37 +0530

    Overlay effect while expanding menu

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,15 +1,18 @@
 
 const headerRight = document.querySelector('.header-right');
 const menuButton = document.querySelector('.menu-button');
+const overlay = document.querySelector('.overlay');
 
 const openMenu = () => {
   headerRight.classList.add('visible');
+  overlay.classList.add('visible');
   headerRight.setAttribute('aria-hidden', 'false');
   menuButton.setAttribute('aria-expanded', 'true');
 }
 
 const closeMenu = () => {
   headerRight.classList.remove('visible');
+  overlay.classList.remove('visible');
   headerRight.setAttribute('aria-hidden', 'true');
   menuButton.setAttribute('aria-expanded', 'false');
 }
@@ -19,7 +22,7 @@ document.addEventListener('click', (event) => {
     openMenu();
     return;
   }
-  if ((event.target.closest('.close-button') || event.target.closest('.main')) && headerRight.classList.contains('visible')) {
+  if ((event.target.closest('.close-button') || event.target.closest('.overlay')) && headerRight.classList.contains('visible')) {
     closeMenu();
   }
 })
