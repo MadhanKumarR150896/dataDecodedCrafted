f9ee394636502b1f2ea9d9368b3b6da38194e124
MadhanKumarR150896 <madhankumar150896@outlook.com>
2026-01-10 22:13:02 +0530

    Markup for instant search results

--- src/homePage/search.js
+++ src/homePage/search.js
@@ -4,10 +4,33 @@ let timeoutId;
 
 export async function renderSearchBoxResults(searchTerm) {
   clearTimeout(timeoutId);
+  const dropDown = document.querySelector(".sr-drop");
 
-  if (searchTerm === "") return;
+  if (searchTerm === "") {
+    dropDown.innerHTML = "";
+    dropDown.classList.remove("searched");
+    return;
+  }
+
   timeoutId = setTimeout(async () => {
-    const response = await fetchSearchBoxResults(searchTerm);
-    console.log(response);
+    const response = await Promise.allSettled([
+      fetchSearchBoxResults(searchTerm),
+    ]);
+
+    let srPosts =
+      response[0]?.status === "fulfilled" && response[0].value !== null
+        ? response[0].value
+        : [];
+
+    if (srPosts.length > 0) {
+      dropDown.classList.add("searched");
+
+      dropDown.innerHTML = srPosts
+        .map((srPost) => `<li><a href="">${srPost.title}</a></li>`)
+        .join("");
+    } else {
+      dropDown.classList.remove("searched");
+      dropDown.innerHTML = "";
+    }
   }, 500);
 }
